a519994de52cb8fa2fc1e899e6c75479d8cf819c
Oracso <85285983+Oracso@users.noreply.github.com>
2024-01-31 19:41:17 +0000

    Added IOS 17 support

--- Sources/ChartsPackage/BarChartView.swift
+++ Sources/ChartsPackage/BarChartView.swift
@@ -1,43 +1,43 @@
-////
-////  BarChartView.swift
-////
-////
-////  Created by Oscar Hardy on 31/01/2024.
-////
 //
-//import SwiftUI
-//import Charts
+//  BarChartView.swift
 //
-//struct BarChartView: View {
-//    
-//    let chartData: ChartData
-//    
-//    var body: some View {
-//        
-//        Chart {
-//            ForEach(chartData.chartItems) { item in
-//                
-//                BarMark(
-//                    x: .value(chartData.xAxis.capitalized, item.key.capitalized),
-//                    y: .value(chartData.yAxis.capitalized, item.value)
-//                )
-//                
-//            }
-//        }
-//        
-//        .chartXAxisLabel(position: .bottom, alignment: .center) {
-//            Text(chartData.xAxis.capitalized).bold()
-//        }
-//        
-//        .chartYAxisLabel {
-//            Text(chartData.yAxis.capitalized).bold()
-//        }
-//        
-//    }
-//}
 //
-//struct BarChartView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        BarChartView(chartData: ChartData.example)
-//    }
-//}
+//  Created by Oscar Hardy on 31/01/2024.
+//
+
+import SwiftUI
+import Charts
+
+struct BarChartView: View {
+    
+    let chartData: ChartData
+    
+    var body: some View {
+        
+        Chart {
+            ForEach(chartData.chartItems) { item in
+                
+                BarMark(
+                    x: .value(chartData.xAxis.capitalized, item.key.capitalized),
+                    y: .value(chartData.yAxis.capitalized, item.value)
+                )
+                
+            }
+        }
+        
+        .chartXAxisLabel(position: .bottom, alignment: .center) {
+            Text(chartData.xAxis.capitalized).bold()
+        }
+        
+        .chartYAxisLabel {
+            Text(chartData.yAxis.capitalized).bold()
+        }
+        
+    }
+}
+
+struct BarChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarChartView(chartData: ChartData.example)
+    }
+}
